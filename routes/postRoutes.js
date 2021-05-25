37b68c198a5da8ad4cb367caf28c78fc16e78db8
sam <sam.leider@hotmail.com>
2021-05-24 20:06:41 -0400

    added posts get single post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,4 +18,14 @@ router.get('/', async (req, res) => {
   const posts = await Post.find({ user: res.userId })
   return res.status(200).json({ message: 'Here are you\'re posts', status: 'success', posts })
 })
+
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    return res.status(200).json({ message: 'Here is the post', status: 'success', post })
+  } catch (error) {
+
+    return res.status(404).json({ message:'Sorry Could not find the post you were looking for', status: 'failure'})
+  }
+})
 module.exports = router;
